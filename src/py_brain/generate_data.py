e188b71cf5e17c0596f07b3bcf4e5c1c25e0e5b5
Avneesh <avneeshyadavipu@gmail.com>
2026-01-11 16:14:28 +0530

    Changed generate_data.py behaviour, trained model in train_model.py and saved model.pkl generated new dataset for later stages.

--- src/py_brain/generate_data.py
+++ src/py_brain/generate_data.py
@@ -1,5 +1,6 @@
 import random 
 import pandas as pd
+import os
 
 #Custom list to define a 'Menu' of possible tasks
 TASK_TEMPLATES = [
@@ -9,37 +10,50 @@ TASK_TEMPLATES = [
     {"name":"Heavy_Compute", "id":3, "base_time":50, "possible_io": (1000,6000)},
 ]
 
-data_rows = []
+#Function to create a synthetic logs.
+def generate_synthetic_logs(num_rows, filename):
+    data_rows = []
+    s_no = 0
 
-s_no = 0
+    for i in range(num_rows):
+        for template in TASK_TEMPLATES:
+            min_io, max_io = template["possible_io"]
+            io_data = random.randint(min_io,max_io)
+            priority = random.randint(1,10)
+            base_exec_time = template["base_time"]
+            noise = random.randint(-50,50)
+            exec_time = base_exec_time*io_data+noise
+            exec_time = max(1, exec_time)
+            
+            row={
+                "Process_ID": 1000+s_no,
+                "Task_Name": template["name"],
+                "Task_ID": template["id"],
+                "Input_Data": io_data,
+                "Priority": priority,
+                "Execution_Time": exec_time
+            }
 
-for i in range(250):
-    for template in TASK_TEMPLATES:
-        min_io, max_io = template["possible_io"]
-        io_data = random.randint(min_io,max_io)
-        priority = random.randint(1,10)
-        base_exec_time = template["base_time"]
-        noise = random.randint(-50,50)
-        exec_time = base_exec_time*io_data+noise
-        exec_time = max(1, exec_time)
-        
-        row={
-            "Process_ID": 1000+s_no,
-            "Task_Name": template["name"],
-            "Task_ID": template["id"],
-            "Input_Data": io_data,
-            "Priority": priority,
-            "Execution_Time": exec_time
-        }
+            data_rows.append(row)
 
-        data_rows.append(row)
+            s_no+=1
 
-        s_no+=1
+    data_rows = data_rows[:num_rows]
+    random.shuffle(data_rows)
 
-random.shuffle(data_rows)
+    generated_tasks_df = pd.DataFrame(data=data_rows)
 
-generated_tasks_df = pd.DataFrame(data=data_rows)
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    output_path = os.path.join(script_dir, f"../../data/{filename}")
 
-print(generated_tasks_df.head(10))
+    generated_tasks_df.to_csv(output_path, index=False)
+    print(f"Generated {filename} with {num_rows} rows.")
+    print(f"Path: {output_path}\n\n")
 
-generated_tasks_df.to_csv("data/raw_logs.csv", index=False)
+#This will only run if this file is executed directly
+if __name__ == "__main__":
+    #These are the synthetic logs we will use to train our model
+    generate_synthetic_logs(1000, "raw_logs.csv")
+
+    #These logs will we used for testing and to simulate 'New' tasks
+    generate_synthetic_logs(500, "incoming_tasks.csv")
---
+++ src/py_brain/train_model.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import os
+import sklearn
+import pickle
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+dataset_path = os.path.join(script_dir, "../../data/raw_logs.csv")
+
+df = pd.read_csv(dataset_path)
+
+X = df[["Task_ID","Input_Data"]]
+y = df["Execution_Time"]
+
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state=42)
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+predictions = model.predict(X_test)
+print(f"Model Accureacy (R2 Score): {r2_score(y_test, predictions)}")
+
+model_path = os.path.join(script_dir, "model.pkl")
+with open(model_path, 'wb') as file:
+    pickle.dump(model, file)
+print(f"Model saved to: {model_path}")
+
+# When we fit our X_train and y_train on the Linear Regression model it calculates two coefficients and one intercept
+#  TIME = (Coef_1 x Task_ID) + (Coef_2 x Input_Data) + Intercept
+# To get an estimated time in our final steps in C we will directly use these coefficients and the intercepts.
+print(f"Formula: ({model.coef_[0]:.4f} * Task_ID) + ({model.coef_[1]:.4f} * Input_Data) + ({model.intercept_:.4f})")
